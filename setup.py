1b97594df4e8a6d62b15e68fa71680c2123ddaf8
Felipe Lerena <felipelerena@gmail.com>
2015-11-30 18:08:49 -0300

    adding python 3 support

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ except ImportError:
 
 setup(
     name='Flask-SQLAlchemySession',
-    version='0.0.2',
+    version='0.0.3',
     author='Felipe Lerena',
     author_email='felipelerena@gmail.com',
     packages=['FlaskSQLAlchemySession'],
